32fac4044887be134142ede8e712daa2a0e0f70c
dhoserv <dho-net@gmx.de>
2013-07-24 17:39:54 +0200

    Hello_World
    Change-Id: Ie683bd862ffe85ac19a048d103df679ad5bbda72
    Signed-off-by: dhoserv <dho-net@gmx.de>

---
+++ Hello_World/src/Hello_World.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Hello_World.cpp
+// Author      : free
+// Version     :
+// Copyright   : free
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello_World" << endl; // prints Hello_World
+	return 0;
+}
